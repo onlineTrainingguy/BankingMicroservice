ad86c3da1344cec3bf888d74db31e898dad82b7b
raman <raman@gmail.com>
2023-01-21 18:10:32 +0000

    new code

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello DevOps Capstone Proj!" );
+        System.out.println( "Hello DevOps Capstone Proj!123" );
     }
 }
